8c1582bd5fe0997563c144f1f1cb77ecc5542ea6
Ambe Hideki <ykc@daycrift.net>
2023-12-28 20:18:38 +0900

    feat: add punch command

---
+++ lib/iamworkingperson.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'watir'
+require 'dotenv'
+require_relative './iamworkingperson/national_holiday'
+
+Dotenv.load
+
+return unless NationalHoliday.new.fetch.include?(Date.today.day) || !ENV.fetch('DEBUG', false)
+
+browser = Watir::Browser.new
+browser.goto 'https://id.jobcan.jp/users/sign_in?app_key=atd'
+browser.text_field(name: 'user[email]').set ENV.fetch('ACCOUNT_NAME')
+browser.text_field(name: 'user[password]').set ENV.fetch('ACCOUNT_PASSWORD')
+browser.button(name: 'commit').click
+
+browser.goto 'https://ssl.jobcan.jp/employee'
+browser.button(name: 'adit_item').click
+
+# debug
+#browser.screenshot.save 'screenshot.png'
+
+browser.close
---
+++ lib/iamworkingperson/national_holiday.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'json'
+require 'date'
+
+class NationalHoliday
+  API_ENDPOINT = 'https://holidays-jp.github.io/api/v1/date.json'
+  private_constant :API_ENDPOINT
+
+  # @param [Date, nil] date
+  def initialize(date: nil)
+    @date = if date
+              date
+            else
+              today = Date.today
+              @date ||= Date.new(today.year, today.month, 1)
+            end
+  end
+
+  def fetch
+    ret = []
+
+    holidays.each do |k, _|
+      t = Date.parse(k)
+      ret << t.day if t.year == date.year && t.month == date.month
+    end
+
+    ret
+  end
+
+  private
+
+  attr_reader :date
+
+  def holidays
+    res = Net::HTTP.get_response(URI.parse(API_ENDPOINT))
+    JSON.parse(res.body)
+  end
+end
